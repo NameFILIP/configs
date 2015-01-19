3679a2766383aac6ae68bbb09257eaf8adf0b1ec
Filip Spiridonov <filip.spiridonov@gmail.com>
2015-01-19 10:22:05 -0800

    Add editorconfig, setup-mac.sh

---
+++ setup-mac.sh
@@ -0,0 +1,9 @@
+# Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew install git
+
+# Show hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# Git
