0e1c677f49db5a7155077e7b55a6340a4afd6e9a
Filip Spiridonov <filip.spiridonov@gmail.com>
2017-10-01 23:25:46 -0700

    Change git checkout alias to gk

--- setup-mac.sh
+++ setup-mac.sh
@@ -45,12 +45,11 @@ export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 # Aliases
 alias ll='ls -lhA'
 alias ps='ps auxf'
-
 alias ga='git add'
 alias gaa='git add --all'
 alias gb='git branch'
 alias gc='git commit'
-alias go='git checkout'
+alias gk='git checkout'
 alias gl='git pull'
 alias gp='git push'
 alias gs='git status'
